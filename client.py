3fd3c182faf6d406adfe5efd9a3e1a060d1161eb
Aditya <adityaraj6508@gmail.com>
2024-03-23 00:51:17 +0530

    Two clients can communicate to each other, turn wise, with E2EE
    The clients currently only talk to each other taking turn. Need to implement better communication(maybe threading)

--- client.py
+++ client.py
@@ -1,6 +1,12 @@
 import socket
 import sys
 
+from Crypto import Random
+from Crypto.Cipher import PKCS1_OAEP
+from Crypto.PublicKey import RSA
+
+Random.atfork()
+
 
 def main(argv):
     if len(argv) != 2:
@@ -10,14 +16,39 @@ def main(argv):
     server_port = 8000
     client_port = int(argv[1])
 
+    BUFFER_SIZE = 2048
+
+    key = RSA.generate(1024)
+    public_key = key.publickey().export_key()
+    private_key = key
+
+    decryptor = PKCS1_OAEP.new(private_key)
+
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.bind((host, client_port))
     client_socket.connect((host, server_port))
-    data = client_socket.recv(1024)
+    data = client_socket.recv(BUFFER_SIZE)
     print(f"Received: {data.decode()}")
+    # print(f"Sending: {public_key.hex()}")
+    client_socket.sendall(public_key)
+    data = client_socket.recv(BUFFER_SIZE)
+    # print(f"Key: {data.hex()}")
+    encryption_key = RSA.import_key(data)
+    encryptor = PKCS1_OAEP.new(encryption_key)
 
-    data = b"Hello, server!"
-    client_socket.sendall(data)
+    print("Enter 'exit' to exit")
+
+    while True:
+        data = input("> ").encode()
+        if data == b"exit":
+            break
+
+        data = encryptor.encrypt(data)
+        # print(f"Sending: {data.hex()}")
+        client_socket.sendall(data)
+        data = client_socket.recv(BUFFER_SIZE)
+        data = decryptor.decrypt(data)
+        print(f"Received: {data.decode()}")
 
     client_socket.close()
 
--- server.py
+++ server.py
@@ -4,30 +4,35 @@ import sys
 
 def main(argv: list[str] = []) -> None:
     port = 8000
+    BUFFER_SIZE = 2048
+
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind(("127.0.0.1", port))
         server_socket.listen(2)
 
         print(f"Server started and listening on port {port}")
 
-        clients = {}
+        clients = []
+
+        for i in range(2):
+            client_socket, addr = server_socket.accept()
+            print(f"Connected by {addr}")
+            client_socket.sendall(f"Hello, client {chr(65+i)}!".encode())
+            encryption_key = client_socket.recv(BUFFER_SIZE)
+            clients.append((addr, client_socket, encryption_key))
+
+        for idx, (addr, client_socket, encryption_key) in enumerate(clients):
+            client_socket.sendall(clients[1 - idx][2])
 
         while True:
-            while len(clients) < 2:
-                client_socket, addr = server_socket.accept()
-                print(f"Connected by {addr}")
-                clients[addr] = client_socket
-
-            for idx, (addr, client_socket) in enumerate(clients.items()):
-                client_socket.sendall(f"Hello, client {chr(65+idx)}!".encode())
-                data = client_socket.recv(1024)
+            for idx, (addr, client_socket, encryption_key) in enumerate(clients):
+                data = client_socket.recv(BUFFER_SIZE)
                 if not data:
                     print(f"Disconnected by {addr}")
-                    clients[addr].close()
+                    client_socket.close()
                     continue
-                print(f"Received: {data.decode()}")
-
-            break
+                print(f"Data from {chr(65+idx)}: {data.hex()}")
+                clients[1 - idx][1].sendall(data)
 
 
 if __name__ == "__main__":
