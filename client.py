9000f495c1f53fa1f0439a562530f8510491adb6
Aditya <adityaraj6508@gmail.com>
2024-03-22 22:48:36 +0530

    Added support for two clients

--- client.py
+++ client.py
@@ -3,12 +3,23 @@ import sys
 
 
 def main(argv):
-    port = 8000
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
-        server_socket.connect(("127.0.0.1", port))
-        server_socket.sendall(b"Hello, world")
-        data = server_socket.recv(1024)
-        print(f"Received {data!r}")
+    if len(argv) != 2:
+        print(f"Usage: {argv[0]} <client port>")
+        sys.exit(1)
+    host = "127.0.0.1"
+    server_port = 8000
+    client_port = int(argv[1])
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.bind((host, client_port))
+    client_socket.connect((host, server_port))
+    data = client_socket.recv(1024)
+    print(f"Received: {data.decode()}")
+
+    data = b"Hello, server!"
+    client_socket.sendall(data)
+
+    client_socket.close()
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -6,16 +6,28 @@ def main(argv: list[str] = []) -> None:
     port = 8000
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.bind(("127.0.0.1", port))
-        server_socket.listen(5)
+        server_socket.listen(2)
 
-        conn, addr = server_socket.accept()
-        with conn:
-            print("Connected by", addr)
-            while True:
-                data = conn.recv(1024)
+        print(f"Server started and listening on port {port}")
+
+        clients = {}
+
+        while True:
+            while len(clients) < 2:
+                client_socket, addr = server_socket.accept()
+                print(f"Connected by {addr}")
+                clients[addr] = client_socket
+
+            for idx, (addr, client_socket) in enumerate(clients.items()):
+                client_socket.sendall(f"Hello, client {chr(65+idx)}!".encode())
+                data = client_socket.recv(1024)
                 if not data:
-                    break
-                conn.sendall(data)
+                    print(f"Disconnected by {addr}")
+                    clients[addr].close()
+                    continue
+                print(f"Received: {data.decode()}")
+
+            break
 
 
 if __name__ == "__main__":
